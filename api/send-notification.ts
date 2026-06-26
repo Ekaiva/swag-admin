efdc197d760478569770437ff5154343735b3cbb
Pramod <pramodsinghiit2016@gmail.com>
2026-06-27 00:27:43 +0530

    fix version 1

--- api/send-notification.ts
+++ api/send-notification.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { sendPushNotification, type Screen } from "./shared/expo-push";
-import { listTokens } from "./shared/token-store";
+import { sendPushNotification, type Screen } from "./shared/expo-push.js";
+import { listTokens } from "./shared/token-store.js";
 
 const VALID_SCREENS: Screen[] = ['1', '2', '3'];
 
